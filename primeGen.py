851012208ef88e996003daf2cfecece838ba3a39
Phoebe Liu <poeticphoebe@gmail.com>
2016-03-16 16:27:04 -0400

    Update primeGen.py

--- primeGen.py
+++ primeGen.py
@@ -1,4 +1,5 @@
 ## ---------------- ##
+## Method 1 (fast)  ##
 ## Taken from http://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n/3035188#3035188 ##
 ## ---------------- ##
 
@@ -9,3 +10,18 @@ def primes(n):
         if sieve[i]:
             sieve[i*i::2*i]=[False]*((n-i*i-1)/(2*i)+1)
     return [2] + [i for i in xrange(3,n,2) if sieve[i]]
+
+
+## ---------------- ##
+## Method 2 (slow)  ##
+## ---------------- ##
+
+def primes(n):
+    prime = []
+    k = range(2,n)
+    while len(k)>0:
+    	i = k[0]
+	    k = [g for g in k if g%k[0] != 0]
+    	prime.append(i)
+	    if i**2 > a:
+		    break
