5b560beaf00489517bb0fc6430244039558cd860
Phoebe Liu <poeticphoebe@gmail.com>
2016-03-16 16:29:28 -0400

    Create primeTest.py

---
+++ primeTest.py
@@ -0,0 +1,9 @@
+## ------------------------------------- ##
+## Purpose: to test if a number is prime ##
+## ------------------------------------- ##
+
+def is_prime(n):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
