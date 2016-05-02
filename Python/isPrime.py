1321c1ba0ac2df72267ddd335e546390f22fd398
Phoebe Liu <poeticphoebe@gmail.com>
2016-05-02 13:41:15 -0400

    Update isPrime.py

--- Python/isPrime.py
+++ Python/isPrime.py
@@ -3,7 +3,7 @@
 ## ------------------------------------- ##
 import math
 def is_prime(n):
-    for i in range(2, int(math.floor(n**0.5))):
+    for i in range(2, int(math.floor(n**0.5)+1)):
         if n % i == 0:
             return False
     return True
