46f52ab0f4859da69aa49ddc0e55f24d958c2f6d
Phoebe Liu <poeticphoebe@gmail.com>
2016-06-02 10:54:09 -0400

    Update genPrime.py

--- Python/genPrime.py
+++ Python/genPrime.py
@@ -7,7 +7,6 @@
 ## http://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n/3035188#3035188 ##
 ## ------------------------------------------------------------------------------------------------- ##
 
-import numpy
 def primes(n):
     """ Returns  a list of primes < n """
     sieve = [True] * n
@@ -28,5 +27,5 @@ def primes(n):
     	i = k[0]
 	k = [g for g in k if g%k[0] != 0]
     	prime.append(i)
-	    if i**2 > a:
-		    break
+	if i**2 > a:
+		break
